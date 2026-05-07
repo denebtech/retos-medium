dc683d019d3f4353a11b01ac1a5986926401258c
Jairo JM Quispe <joelquispeunju@gmail.com>
2026-05-07 18:56:45 -0300

    Python - Reto 1.1
    - Mejoras en la funcionalidad

--- python/reto1.1/main.py
+++ python/reto1.1/main.py
@@ -1,11 +1,18 @@
 def menu():
-    print("Calculadora de IMC (Índice de Masa Corporal)")
     print("-" * 50)
+    print("Calculadora de IMC (Índice de Masa Corporal)")
     
-    print("Sistema a emplear")
-    print("1. Sistema métrico")
-    print("2. Sistema inglés")
-    sistema = int(input("Ingrese una de las opciones mencionadas: "))
+    while True:
+        print("-" * 50)
+        print("Sistema a emplear")
+        print("1. Sistema métrico")
+        print("2. Sistema inglés")
+        sistema = int(input("Ingrese una de las opciones mencionadas: "))
+
+        if sistema in [1, 2]:
+            break
+        else:
+            print("Opción no válida. Intente nuevamente.")
 
     print("-" * 50)
     peso = float(input("Ingrese su peso: "))
@@ -24,17 +31,8 @@ def calculadora_inglesa(peso, altura):
     return imc
 
 
-if __name__ == "__main__":
-    sistema, peso, altura = menu()
-
-    if sistema == 1:
-        imc = calculadora_metrica(peso, altura)
-    elif sistema == 2:
-        imc = calculadora_inglesa(peso, altura)
-    else:
-        print("Opción no válida.")
-        exit()
-
+def diagnostico(imc):
+    print("-" * 50)
     print(f"Tu IMC es: {imc:.2f}")
     if imc < 18.5:
         print("Categoría: Bajo peso")
@@ -44,3 +42,22 @@ if __name__ == "__main__":
         print("Categoría: Sobrepeso")
     else:
         print("Categoría: Obesidad")
+
+
+if __name__ == "__main__":
+
+    while True:
+        sistema, peso, altura = menu()
+
+        if sistema == 1:
+            imc = calculadora_metrica(peso, altura)
+            diagnostico(imc)
+        if sistema == 2:
+            imc = calculadora_inglesa(peso, altura)
+            diagnostico(imc)
+
+        print("-" * 50)
+        continuar = input("¿Desea calcular otro IMC? (s/n): ")
+        if continuar.lower() != 's':
+            print("¡Gracias por usar la calculadora de IMC!")
+            break
